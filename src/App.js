8b538b25a640a172e7045d99faf7412af1a4aeca
gorbatkoff <gorbatkovartyom@gmail.com>
2022-01-28 10:16:33 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,110 +1,77 @@
 import { useState, useEffect } from 'react';
-import Header from './components/Header';
 import ToDoForm from './components/ToDoForm';
 import TaskList from './components/TaskList';
 import Sort from './components/Sort';
-
-import { Pagination, Row } from 'antd';
-import { Divider } from 'antd';
+import { Pagination, Row, Divider } from 'antd';
+import axios from 'axios';
 
 function App() {
 
-  const [todos, setTodos] = useState([]); // Array of the tasks
-  const [currentTasks, setCurrentTasks] = useState(todos); // Copy of array
-
-  const [type, setType] = useState('All'); // Types of sort by status 
-  const [sortType, setSortType] = useState('reverse'); // Types of sort by date
-
+  const [filteredTodos, setFilteredTodos] = useState([]); // Array of the tasks
   const [currentPage, setCurrentPage] = useState(1); // State of current page
-  const [amountPages, setAmountPages] = useState(1); // Amount of pages
+  const [status, setStatus] = useState('all') // State for sorting by status
+  const [date, setDate] = useState('desc'); // State for sorting by date
+  const [todosCount, setTodosCount] = useState(0); // State of amount tasks
+  const [todosPerPage] = useState(5); // Const of max tasks per page
+  
+  const api = axios.create({
+    baseURL: 'https://todo-api-learning.herokuapp.com/v1/',
+  });
 
-  const maxTasks = 5; // Max tasks on any page
-
-  // =-------------------------------------------------------------------------=
-
+  // useEffect in which we get an array of current Tasks 
   useEffect(() => {
-    const filtredTasks = todos.filter(task => selectOption(task, type));
+    getTodos(); // here we call function to get an array of current Tasks
+  }, [status, date, currentPage]); // here i announced states, which need to us for re-render page
 
-    const outputTasks = filtredTasks
-      .sort((a, b) => sortByDate(a, b, sortType))
-      .slice((currentPage - 1) * maxTasks, currentPage * maxTasks);
 
-    const pagesFraction = filtredTasks.length / maxTasks;
+  const getTodos = async () => { // async function to get an array of tasks from API
 
-    let amountPages = filtredTasks.length % maxTasks === 0 ? pagesFraction : Math.floor(pagesFraction) + 1;
-
-    if (amountPages < 1) {
-      setType('All')
-      amountPages = 1;
-    }
-
-    if (amountPages < currentPage) {
-      setCurrentPage(amountPages)
-    }
-
-    setAmountPages(amountPages);
-    setCurrentTasks(outputTasks);
-
-  }, [todos, type, sortType, currentPage]);
-
-  function selectOption(task, type) {
-    switch (type) {
-      case 'All':
-        return true
-
-      case 'Done':
-        if (task.complete)
-          return true;
-        return false;
-
-      case "Undone":
-        if (task.complete)
-          return false;
-        return true;
-    }
-  }
-  function sortByStatus(arg) {
-    setType(arg);
-    // console.log(arg)
-  }
-  function sortingTasks(arg) {
-    setSortType(arg);
-  }
-  function sortByDate(a, b, sortType) {
-    if (sortType === 'standart') {
-      if (a < b)
-        return 1;
-      return -1;
-    }
-
-    if (sortType === 'reverse') {
-      if (a > b)
-        return -1;
-      return 1;
-    }
-  }
-  function deleteTask(id) {
-    setTodos([...todos.filter((todo) => todo.id !== id)])
-  }
-  function getDone(taskId) {
-    setTodos([...todos.map(item => item.id === taskId ? { ...item, complete: !item.complete } : item)])
-  }
-  function changeTask(taskId, text) {
-    setTodos([...todos.map(task => task.id === taskId ? { ...task, task: text } : task)]);
-  }
-  function addTask(userInput) {
-    if (userInput) {
-      const newTask = {
-        id: Math.random().toString(10).substring(2, 9),
-        task: userInput,
-        complete: false,
-        time: +new Date()
+    const response = await api.get(`/tasks/5`, { // The start of response
+      params: { // here we add values to our states
+        filterBy: status === 'all' ? '' : status, // here sorting by status
+        order: date, // here sorting by date
+        pp: todosPerPage, // here we add value to postsPerPage
+        page: currentPage // here we add currentPage
       }
-      setTodos([newTask, ...todos]);
-    }
+    })
+    if (response.data.tasks.length === 0 && currentPage > 1){ 
+      setCurrentPage(currentPage - 1)}; // setting current page 
+    setTodosCount(response.data.count); // set response.data.count to our tasks count
+    setFilteredTodos(response.data.tasks); // set tasks to FilteredTodos State
+  };
+
+  const addTask = async(input) => { // here i declarate function for creating new Task and post it to server
+    const newTask = { // newTask object
+      name: input, // input which user write in input form
+      done: false // done false 'cause it's logic
+    };
+
+    await api.post(`task/5`, newTask); // sending a post request to the server
+    await getTodos(); // get updated list of Tasks
   }
-  function changeCurrentPage(pageNumber) {
-    setCurrentPage(pageNumber)
+
+  const deleteTask = async(e, id) => {
+    e.currentTarget.disabled = true;
+    await api.delete(`/task/5/${id}`);
+    await getTodos();
+  }
+
+  const changeTask = async(text, id) => {
+    let req = await api.patch(`/task/5/${id}`, text); // sending new task to API
+    await getTodos(); // rerendering
+    
+    console.log("change task")
+    
+    return req;
+  }
+
+  const sortByStatus = async (val) => setStatus(val);
+
+  const sortByDate = async (val) => setDate(val)
+
+  const paginate = (num) => {
+    getTodos();
+    setCurrentPage(num);
   }
 
   return (
@@ -115,27 +82,23 @@ function App() {
 
       <Sort
         sortByStatus={sortByStatus}
-        sortingTasks={sortingTasks}
+        sortByDate={sortByDate}
       />
 
       <TaskList
-        currentTasks={currentTasks}
+        filteredTodos={filteredTodos}
         changeTask={changeTask}
         deleteTask={deleteTask}
-        getDone={getDone}
       />
-
-      {/* <Pagination
-        amountPages={amountPages}
-        changeCurrentPage={changeCurrentPage}
-        currentPage={currentPage}
-      /> */}
 
       <Row justify="center">
         <Pagination
           defaultCurrent={1}
-          total={50}
-          onChange={changeCurrentPage}
+          total={ todosCount }
+          onChange={paginate}
+          hideOnSinglePage={true}
+          current={currentPage}
+          pageSize={todosPerPage}
         />
       </Row>
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,16 @@
 import { useState } from "react/cjs/react.development";
-import { Checkbox, Input, Button, Row, Col} from 'antd';
+import { Checkbox, Input, Button, Row, Col } from 'antd';
 
-function Task({ task, changeTask, getDone, deleteTask }) {
+function Task({ task, changeTask, deleteTask }) {
 
     const [editRegime, setEditRegime] = useState(false);
-    const [taskText, setTaskText] = useState(task.task);
+    const [taskText, setTaskText] = useState(task.name);
     let wasChanged = false;
 
     function handleOnBlur() {
         setEditRegime(false)
         if (!wasChanged)
-            setTaskText(task.task);
+            setTaskText(task.name);
         wasChanged = false;
     }
 
@@ -24,13 +24,13 @@ function Task({ task, changeTask, getDone, deleteTask }) {
 
     const handleKeyDown = (e) => {
         if (e.keyCode == 13) {
-            changeTask(task.id, e.currentTarget.value);
+            changeTask(e.currentTarget.value, task.uuid);
             wasChanged = true;
             e.currentTarget.blur();
         }
 
         if (e.keyCode == 27) {
-            setTaskText(task.task)
+            setTaskText(task.name)
             e.currentTarget.blur();
         }
     }
@@ -48,33 +48,41 @@ function Task({ task, changeTask, getDone, deleteTask }) {
             onChange={handleOnChange}
             onKeyDown={handleKeyDown}
             onBlur={handleOnBlur}
-        >
-
-        </Input>
+        />
     )
 
+    // const handlerSubmit = (taskText) => {
+    //     alert(task.text);
+    // }
+
+    const getDone = () => {
+        task.done = !task.done;
+        changeTask(task, task.uuid)
+    }
+
     return (
-        <Row align="middle" justify="space-between" key={task.id}>
+        <Row align="middle" justify="space-between" key={task.uuid}>
 
             <Col span={1}>
                 <Checkbox
                     type="checkbox"
-                    // id={task.id}
-                    onChange={() => getDone(task.id)}
-                    checked={task.complete}>
+                    onChange={getDone}
+                    checked={task.done}>
                 </Checkbox>
             </Col>
 
-            <Col span={18}>
+            <Col span={18}
+            // onChange={handlerSubmit}
+            >
                 {editRegime ? edit : look}
             </Col>
 
             <Col span={2}>
-                {new Date(task.time).toLocaleString('ru-ru')}
+                {new Date(task.createdAt).toLocaleString('ru-ru')}
             </Col>
 
             <Col span={1}>
-                <Button onClick={() => deleteTask(task.id)}>Delete</Button>
+                <Button onClick={(e) => deleteTask(e, task.uuid)}>Delete</Button>
             </Col>
         </Row>
     )
