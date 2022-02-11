7217499360ccf22265307986b2bc761f36bc2e1c
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-11 17:32:45 +0300

    Create login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Form, Input, Button, Checkbox, Row } from 'antd';
+import { NavLink } from 'react-router-dom';
+
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <Form
+      style={{width: "80%"}}
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+
+      <Row align="middle" justify="space-between" style={{width: "300px"}}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        
+        <NavLink to="../register">Dont have an account yet? Sign Up</NavLink>
+        {/* <div><a>Dont have an account yet?</a></div> */}
+        </Row>
+      </Form.Item>
+    </Form>
   )
 }
 
